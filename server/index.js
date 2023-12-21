a0a8c62e986868958be2a835af55cbf223e99147
Dharun <dharundeepak1819@gmail.com>
2023-12-21 19:50:26 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get("/*",function(req,res){
     res.sendFile(
         path.join(__dirname,"../client/build/index.html"),
         function(err){
-            res.status(500).send(err);
+            res.status(500).send(err); 
         }
     )
 });
